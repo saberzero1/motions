507eb20050985177aeb2e0c127fe2b6e9604855e
saberzero1 <github@emilebangma.com>
2026-07-11 15:19:21 +0200

    chore: improve version bump

--- justfile
+++ justfile
@@ -1,10 +1,12 @@
 default:
-	just --list
+    just --list
 
 bump version:
-	npm_package_version={{version}} node version-bump.mjs
-	npm i -D
+    npm run lint
+    npm_package_version={{version}} node version-bump.mjs
+    npm i -D
+    prettier . --check --write
 
 tag version:
-	git tag -a "{{version}}" -m "Release version {{version}}"
-	git push origin tag "{{version}}"
+    git tag -a "{{version}}" -m "Release version {{version}}"
+    git push origin tag "{{version}}"
--- version-bump.mjs
+++ version-bump.mjs
@@ -3,15 +3,37 @@ import { readFileSync, writeFileSync } from 'fs';
 const targetVersion = process.env.npm_package_version;
 
 // read minAppVersion from manifest.json and bump version to target version
-const manifest = JSON.parse(readFileSync('manifest.json', 'utf8'));
+let manifest = JSON.parse(readFileSync('manifest.json', 'utf8'));
 const { minAppVersion } = manifest;
 manifest.version = targetVersion;
 writeFileSync('manifest.json', JSON.stringify(manifest, null, '\t'));
 
 // update versions.json with target version and minAppVersion from manifest.json
-// but only if the target version is not already in versions.json
-const versions = JSON.parse(readFileSync('versions.json', 'utf8'));
-if (!(targetVersion in versions)) {
-    versions[targetVersion] = minAppVersion;
-    writeFileSync('versions.json', JSON.stringify(versions, null, '\t'));
-}
+let versions = JSON.parse(readFileSync('versions.json', 'utf8'));
+versions[targetVersion] = minAppVersion;
+writeFileSync('versions.json', JSON.stringify(versions, null, '\t'));
+
+// update package.json with target version
+let packageJson = JSON.parse(readFileSync('package.json', 'utf8'));
+packageJson.version = targetVersion;
+writeFileSync('package.json', JSON.stringify(packageJson, null, '\t'));
+
+// update CHANGELOG.md with header for target version
+// turns:
+//
+// ## [Unreleased]
+//
+// Into:
+//
+// ## [Unreleased]
+//
+// ## [targetVersion] - YYYY-MM-DD
+let changelog = readFileSync('CHANGELOG.md', 'utf8');
+const today = new Date().toISOString().split('T')[0];
+const unreleasedHeader = '## [Unreleased]';
+const newVersionHeader = `## [${targetVersion}] - ${today}`;
+changelog = changelog.replace(
+    unreleasedHeader,
+    `${unreleasedHeader}\n\n${newVersionHeader}`,
+);
+writeFileSync('CHANGELOG.md', changelog);
